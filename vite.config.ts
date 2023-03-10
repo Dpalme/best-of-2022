0ffa04f65bc77fdd1db8af0762725a84d19472a8
Dpalme <diegoPalmerin24@gmail.com>
2023-03-09 22:08:00 -0600

    multiple entrypoints

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,9 +7,21 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          appSong: fileURLToPath(
+            new URL('./songs/index.html', import.meta.url)
+          ),
+          appAlbum: fileURLToPath(
+            new URL('./albums/index.html', import.meta.url)
+          ),
+        },
+      },
+    },
     base: env.BASE_URL || '',
     server: {
-      port: 5550
-    }
+      port: 5550,
+    },
   };
 });
